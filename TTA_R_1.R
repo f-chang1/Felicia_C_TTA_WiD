2ca90fa1787070cf7a9078f9dd9bba5526dd142c
f-chang1 <50489466+f-chang1@users.noreply.github.com>
2021-03-01 21:35:21 +0000

    Tasks for week 1 of R

---
+++ TTA_R_1.R
@@ -0,0 +1,20 @@
+# task 1
+
+name <- readline("Please enter your name: ")
+age <- readline("Please enter your age: ")
+print(paste("Your name is: ", name))
+print(paste("Your age is: ", age))
+
+# task 2
+
+print(ls.str())
+
+# task 3
+seq_1 <- 20:50
+print(seq_1)
+
+print(mean(20:60))
+print(sum(51:91))
+
+# task 4
+print(sample(-50:50, 10, replace=T))
